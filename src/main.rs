f22995fb5d65969ea575a1eeb47eac729c79b318
istareatscreens <s.charb@gmail.com>
2024-09-04 08:40:49 -0400

    Improve formatting and add explicit port close

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
+use serialport::{DataBits, FlowControl, Parity, StopBits};
 use std::time::Duration;
-use serialport::{DataBits, Parity, StopBits, FlowControl};
 
 fn main() {
     let packet_data: [&[u8]; 6] = [
         &[0xE0, 0x11, 0x01, 0x01, 0x10, 0x23],
         &[0xE0, 0x11, 0x01, 0x01, 0x10, 0x23],
         &[0xE0, 0x11, 0x01, 0x01, 0x10, 0x23],
-        &[0xE0, 0x11, 0x01, 0x08, 0x32, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x6C],
+        &[
+            0xE0, 0x11, 0x01, 0x08, 0x32, 0x00, 0x20, 0x00, 0x00, 0x00, 0x00, 0x00, 0x6C,
+        ],
         &[0xE0, 0x11, 0x01, 0x04, 0x39, 0x00, 0x00, 0x00, 0x4F],
         &[0xE0, 0x11, 0x01, 0x01, 0x3C, 0x4F],
     ];
 
-    let mut button_led_com = serialport::new ("COM21", 115200)
+    let mut button_led_com = serialport::new("COM21", 115200)
         .data_bits(DataBits::Eight)
         .flow_control(FlowControl::None)
         .stop_bits(StopBits::One)
@@ -20,13 +22,12 @@ fn main() {
         .open()
         .expect("Failed to open port");
 
-        packet_data
-            .iter()
-            .for_each(|&bytes| {
-                match button_led_com.write(bytes){
-                    Ok(_) => println!("Successfully wrote packet to serial port."),
-                    Err(e) => eprintln!("Failed to write to serial port: {}", e),
-                }
-            }
-        );
+    packet_data
+        .iter()
+        .for_each(|&bytes| match button_led_com.write(bytes) {
+            Ok(_) => println!("Successfully wrote packet to serial port."),
+            Err(e) => eprintln!("Failed to write to serial port: {}", e),
+        });
+
+    std::mem::drop(button_led_com);
 }
